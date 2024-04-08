eea5fd100b25f7c2b3b4cd72f85bb392f8a9c2e5
rishurana8 <rishurana256@gmail.com>
2024-04-09 01:13:27 +0530

    fetched data from api in real time

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,7 +7,7 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{width: "18rem"}}>
-          <img src="{imageUrl}" className="card-img-top" alt="..." />
+          <img src={!imageUrl?"https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcQRt_0WRr8Mc016RGaTK8eaiv6dSHKuNjIwdUrnF_7Xa_GdQL9YX9f4le5qucuyVUpKxbo7gqIGC0pZo14":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">
