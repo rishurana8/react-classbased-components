8214bc1e9391fdc544b8d5a9c8583cf6529913a5
rishurana8 <rishurana256@gmail.com>
2024-04-11 01:12:26 +0530

    added pagination by passing number of pages through props

--- src/components/News.js
+++ src/components/News.js
@@ -13,7 +13,7 @@ export class News extends Component {
   }
   async componentDidMount(){
     //  yeh method render ke baad mai run hota hai 
-    let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=b74ac0d09949435cb22470e5f8274d8b";
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=b74ac0d09949435cb22470e5f8274d8b&page=1&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     // console.log(data);
     let parsedData = await data.json();
@@ -21,7 +21,7 @@ export class News extends Component {
     this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults});
   }
    handlePrevClick = async()=>{
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=b74ac0d09949435cb22470e5f8274d8b&page=${this.state.page - 1}&pageSize=20`;
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=b74ac0d09949435cb22470e5f8274d8b&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     // console.log(data);
     let parsedData = await data.json();
@@ -33,21 +33,25 @@ export class News extends Component {
        })
   }
    handleNextClick = async()=>{
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=b74ac0d09949435cb22470e5f8274d8b&page=${this.state.page + 1}&pageSize=20`;
-    let data = await fetch(url);
-    // console.log(data);
-    let parsedData = await data.json();
-    console.log(parsedData);
-    // this.setState({articles: parsedData.articles});
-       this.setState({
-          page: this.state.page + 1,
-          articles: parsedData.articles
-       })
+    if(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)){
+        
+    }else{
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=b74ac0d09949435cb22470e5f8274d8b&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+        let data = await fetch(url);
+        // console.log(data);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        // this.setState({articles: parsedData.articles});
+          this.setState({
+              page: this.state.page + 1,
+              articles: parsedData.articles
+          })
+    }
   }
   render() {
     return (
       <div className="container my-3">
-        <h2>NewsFeeder - Top headlines</h2>
+        <h1 className="text-center">NewsFeeder - Top headlines</h1>
         <div className="row">
         {this.state.articles.map((element)=>{
             return <div className="col-md-4" key={element.url}>
@@ -56,8 +60,9 @@ export class News extends Component {
         })}
         </div>
         <div className="container d-flex justify-content-between">
-        <button disabled = {this.state.page<=1} type="button" class="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
-        <button type="button" class="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+        <button disabled = {this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+        <button disabled = {this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+        {/* so what it will do is jab next last page pe hoga toh woh disable ho jaega */}
         </div>
       </div>
     );
